4a3c4d8ff5731265267bce355fedd57601b0665d
CCulver831 <c.culver831@gmail.com>
2020-05-24 19:54:17 -0700

    Enemy shoting WIP. Bullets spawn, but in the wrong direction :( Will debug in the next update

---
+++ GOAP-Projectv1/Assets/BulletPrefab.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPrefab : MonoBehaviour
+{
+    private float speed = 8f;
+
+
+    private void Update()
+    {
+        //Make Bullet move forward
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
--- GOAP-Projectv1/Assets/Scripts/New Actions/ShootAtPlayer.cs
+++ GOAP-Projectv1/Assets/Scripts/New Actions/ShootAtPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ShootAtPlayer : GAction
 {
+    public GameObject BulletSpawn;
+    public GameObject bullet;
     public override bool PrePerform()
     {
         Debug.Log("I have a weapon and I want to shoot you");
@@ -12,6 +14,8 @@ public class ShootAtPlayer : GAction
 
     public override bool PostPerform()
     {
+        GameObject bulletObject = Instantiate(bullet);
+        bulletObject.transform.position = BulletSpawn.transform.position;
         Debug.Log("I am attacking you");
         return true;
     }
