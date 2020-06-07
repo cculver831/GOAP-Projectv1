ace4819cb5ebcb4b7239c9d076ee453c54533895
CCulver831 <c.culver831@gmail.com>
2020-06-06 21:21:59 -0700

    will need to change belief system to account for Ammunition

--- GOAP-Projectv1/Assets/Scripts/New Actions/ShootAtPlayer.cs
+++ GOAP-Projectv1/Assets/Scripts/New Actions/ShootAtPlayer.cs
@@ -6,14 +6,23 @@ public class ShootAtPlayer : GAction
 {
     public GameObject BulletSpawn;
     public GameObject bullet;
-
+    private int Ammo = 6;
 
     public override bool PrePerform()
     {
 
-
+        if(Ammo > 0)
+        {
+            beliefs.AddStateOnce("HasNoAmmo", 0);
+            return true;
+        }
+          
+        else
+        {
+            return false;
+        }
         //Invoke("Fire", 0.5f);
-        return true;
+
     }
     private void Start()
     {
@@ -21,6 +30,7 @@ public class ShootAtPlayer : GAction
     }
     public override bool PostPerform()
     {
+       
         //beliefs.RemoveState("SeesPlayer");
         //CancelInvoke("Fire");
         GameObject bulletObject = Instantiate(bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation);
